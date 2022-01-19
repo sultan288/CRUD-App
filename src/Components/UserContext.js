42158df69844406e0ddeff55681bf93f40222f8b
sultan288 <sultan.ist28@gmail.com>
2022-01-19 14:37:25 +0600

    update crudapp 19jan

--- src/Components/UserContext.js
+++ src/Components/UserContext.js
@@ -7,6 +7,8 @@ export function UserProvider(props) {
         { id: 1, name: 'Salman', position: 'Front End Dev', salary: 28000 },
         { id: 2, name: 'Riad', position: 'Back End Dev', salary: 35000 },
         { id: 3, name: 'Sujon', position: 'Full Stack Dev', salary: 40000 },
+        { id: 4, name: 'Tipu', position: 'Software Engineer', salary: 45000 },
+        { id: 5, name: 'Dipu', position: 'QA Engineer', salary: 40000 },
     ]);
     return <UserContext.Provider value={[users, setUsers]}>{props.children}</UserContext.Provider>;
 }
